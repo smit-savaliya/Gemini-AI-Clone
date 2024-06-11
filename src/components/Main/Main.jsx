5c4b2b59e6b878c5829f159c34b7cd6a7f72a0ea
smit-savaliya <smit.savaliya1717@gmail.com>
2024-06-11 14:27:23 +0530

    add css

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,13 +10,13 @@ function Main() {
   //   console.log(e)
   // })
 
-  const handleKeypress = (e)=>{
-    if(e.key === "Enter"){
+  const handleKeypress = (e) => {
+    if (e.key === "Enter") {
       onSent()
     }
   }
 
-  
+
   return (
     <>
       <div className="main">
@@ -57,46 +57,46 @@ function Main() {
 
               </>
               : <div className='result'>
-                    <div className='result-title'>
-                      <img src={assets.user_icon} alt="" />
-                      <p>{recentPrompt}</p>
+                <div className='result-title'>
+                  <img src={assets.user_icon} alt="" />
+                  <p>{recentPrompt}</p>
 
+                </div>
+                <div className='result-data'>
+                  <img src={assets.gemini_icon} alt="" />
+                  {
+                    loding ? <div className="loader">
+                      <hr />
+                      <hr />
+                      <hr />
                     </div>
-                    <div className='result-data'>
-                          <img src={assets.gemini_icon} alt="" />
-                          {
-                            loding? <div className="loader">
-                                  <hr />
-                                  <hr />
-                                  <hr />
-                            </div>
-                            :<p dangerouslySetInnerHTML={{__html:resultData}}></p>
-                          }
-                          
-                    </div>
-                 </div>
-          }
+                      : <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+                  }
 
-                <div className="main-bottom">
-                  <div className="search-box">
-                    <input onKeyPress={handleKeypress}  onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here' />
-                    <div>
-                      <img src={assets.gallery_icon} alt="" />
-                      <img src={assets.mic_icon} alt="" />
-                      {input ?<img id='sentBtn' onClick={()=> onSent()} src={assets.send_icon} alt="" />:null}
-                      
-                    </div>
-
-                  </div>
-
-                  <p className="bottom-info">
-                    Copyright - thesmitsavaliya
-                  </p>
                 </div>
               </div>
+          }
+
+          <div className="main-bottom">
+            <div className="search-box">
+              <input onKeyPress={handleKeypress} onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder='Enter a prompt here' />
+              <div>
+                <img src={assets.gallery_icon} alt="" />
+                <img src={assets.mic_icon} alt="" />
+                {input ? <img id='sentBtn' onClick={() => onSent()} src={assets.send_icon} alt="" /> : null}
+
+              </div>
+
+            </div>
+
+            <p className="bottom-info">
+              Copyright - thesmitsavaliya
+            </p>
+          </div>
+        </div>
       </div>
-      </>
-      )
+    </>
+  )
 }
 
-      export default Main
+export default Main
